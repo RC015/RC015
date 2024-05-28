11a3daca63e8f4eeb2ff7eb257e0ddc3fa8a8702
Ubuntu <ubuntu@ip-172-31-27-220.ec2.internal>
2024-05-28 07:27:52 +0000

    2nd

--- remot_push.sh
+++ remot_push.sh
@@ -2,3 +2,12 @@ This is a test file
 
 thank you
 
+
+i am able to push the code to the remote repository
+
+what was done?
+
+i generated the public key on my linux/ubuntu machine
+added the key to the git respository security ssh
+
+
